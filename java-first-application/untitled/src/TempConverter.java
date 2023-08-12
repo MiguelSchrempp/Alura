767a6103227e70e787f12d9a5af3f86e43bac8c7
MiguelSchrempp <miguel.schrempp@sptech.school>
2023-08-11 23:19:32 -0300

    adding activity

---
+++ java-first-application/untitled/src/TempConverter.java
@@ -0,0 +1,10 @@
+public class TempConverter {
+    public static void main(String[] args) {
+        double celsio = 22.36;
+        double fahrenheit = (celsio * 1.8) + 32;
+
+        System.out.println("------Temperaturas------");
+        System.out.println("Temperatura em graus celsius: " + celsio);
+        System.out.printf("Temperatura em graus fahreinheit: %.2f", fahrenheit);
+    }
+}
