0797634e888ee73d8029d5942275f41a0ed6f833
MiguelSchrempp <miguel.schrempp@sptech.school>
2023-08-13 11:15:40 -0300

    adding encapsulation

--- applying-oop/src/Main.java
+++ applying-oop/src/Main.java
@@ -1,19 +1,20 @@
+import applying.oop.entidade.Filme;
+
 public class Main {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
 
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.dataDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
-        meuFilme.inclusoNoPlano = true;
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setDataDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.setInclusoNoPlano(true);
 
         meuFilme.exibeDadosDoFilme();
         meuFilme.avaliarFilme(8);
         meuFilme.avaliarFilme(5);
         meuFilme.avaliarFilme(10);
 
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.obterMediaDasNotas());
     }
 }
