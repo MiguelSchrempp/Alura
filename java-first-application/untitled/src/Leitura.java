498245d43c40c0f5bd1edd46a86ff44af2407a5b
MiguelSchrempp <miguel.schrempp@sptech.school>
2023-08-12 22:56:47 -0300

    adding for loop and renaming scanner to Leitura

--- java-first-application/untitled/src/Leitura.java
+++ java-first-application/untitled/src/Leitura.java
@@ -1,6 +1,8 @@
-public class Scanner {
+import java.util.Scanner;
+
+public class Leitura {
     public static void main(String[] args) {
-        java.util.Scanner sc = new java.util.Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Digite seu filme favorito");
         String filmeFavorito = sc.nextLine();
---
+++ java-first-application/untitled/src/OtherLoop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class OtherLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double nota = 0;
+        double media = 0;
+        int totalDeNotas = 0;
+
+        while (nota != -1) {
+            System.out.println("Sua avaliação para o filme ou -1 para encerrar");
+            nota = sc.nextDouble();
+            if (nota != -1) {
+                media += nota;
+                totalDeNotas++;
+            }
+        }
+
+        System.out.println("Média de avaliações: " + media / totalDeNotas);
+    }
+}
