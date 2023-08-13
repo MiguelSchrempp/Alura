569886f1a9c8b0d2ef17189cddfb57e006f94039
MiguelSchrempp <miguel.schrempp@sptech.school>
2023-08-12 23:09:26 -0300

    adding informer

--- java-first-application/untitled/src/Advinhacao.java
+++ java-first-application/untitled/src/Advinhacao.java
@@ -15,10 +15,16 @@ public class Advinhacao {
             if (insertedNumber == randomNumber) {
                 System.out.println("Parabéns você acertou!!");
                 break;
+            } else if (insertedNumber < randomNumber){
+                System.out.println("O número secreto é maior");
             } else {
-                System.out.println("Não era este número :(");
+                System.out.println("O número secreto é menor");
             }
             counter++;
+        }
+
+        if (counter == 5){
+            System.out.println("Acabou e você não acertou =(. O número era: " + randomNumber);
         }
     }
 }
