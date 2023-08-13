9ca56204009f3a7ec48410af9dbc682cf6f4ac7d
MiguelSchrempp <miguel.schrempp@sptech.school>
2023-08-12 22:49:49 -0300

    adding for loop

---
+++ java-first-application/untitled/src/Loop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double nota;
+        double media = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Sua avaliação para o filme");
+            nota = sc.nextDouble();
+            media += nota;
+        }
+
+        System.out.println("Média de avaliações: " + media/3);
+    }
+}
